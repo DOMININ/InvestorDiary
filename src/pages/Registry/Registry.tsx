4c5d839ca3ad3aeb12915d4717228fcb7ab0aab7
Dmitry Dolgushin <razor9714@gmail.com>
2020-12-25 10:39:00 +0500

    added a popup about successful registration

--- src/pages/Registry/Registry.tsx
+++ src/pages/Registry/Registry.tsx
@@ -11,7 +11,9 @@ import VisibilityOffIcon from "@material-ui/icons/VisibilityOff";
 import VisibilityIcon from "@material-ui/icons/Visibility";
 import { useHttp } from "../../hooks/useHttp";
 import useRegistryStyles from "../Registry/theme";
-import { useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
+import Popup from "../../Components/Popup/popup";
+const classNames = require("classnames");
 
 interface IUser {
   email: string;
@@ -20,16 +22,16 @@ interface IUser {
 
 const Registry: React.FC = () => {
   const classes = useRegistryStyles();
-  console.log(classes);
   const [visiblePassword, setVisiblePassword] = useState(false);
   const [form, setForm] = useState<IUser>({ email: "", password: "" });
   const [fieldError, setFieldError] = useState<IUser>({
     email: "",
     password: "",
   });
+  const { email, password } = fieldError;
   const { request, error, loading } = useHttp();
+  const [isRegistered, setIsRegistered] = useState(false);
   const isInitialMount = useRef(true);
-  const history = useHistory();
 
   useEffect(() => {
     if (isInitialMount.current) {
@@ -54,106 +56,116 @@ const Registry: React.FC = () => {
     try {
       const response = await request("POST", "/api/auth/register", { ...form });
       if (response.status === 201) {
-        //TODO: тут добавить попап об успешной регистрации
-        alert("Пользователь создан");
-        history.push("/auth");
+        setIsRegistered(true);
       }
     } catch (e) {}
   };
 
-  const { email, password } = fieldError;
-
   return (
-    <form action="">
-      <Grid container className={classes.grid}>
-        <Box
-          boxShadow={3}
-          width="25%"
-          borderRadius="borderRadius"
-          className={classes.box}
-        >
-          <Typography
-            variant="h4"
-            component="h5"
-            className={classes.typography}
-          >
-            Регистрация нового пользователя
-          </Typography>
-          <TextField
-            label="Фамилия"
-            className={classes.textField}
-            InputProps={{
-              classes: {
-                input: classes.emailInput,
-              },
-            }}
-            fullWidth={true}
-            type="text"
-          />
-          <TextField
-            label="Имя"
-            className={classes.textField}
-            InputProps={{
-              classes: {
-                input: classes.emailInput,
-              },
-            }}
-            fullWidth={true}
-            type="text"
-          />
-          <TextField
-            label="Почта"
-            className={classes.textField}
-            InputProps={{
-              classes: {
-                input: classes.emailInput,
-              },
-            }}
-            helperText={email ? email : ""}
-            error={!!email}
-            onChange={changeHandler}
-            name="email"
-            fullWidth={true}
-            type="email"
-          />
-          <div className={classes.passwordDiv}>
-            <TextField
-              label="Пароль"
-              className={classes.textField}
-              InputProps={{
-                classes: {
-                  input: classes.passwordInput,
-                },
-              }}
-              helperText={password ? password : ""}
-              error={!!password}
-              onChange={changeHandler}
-              name="password"
-              fullWidth={true}
-              type={visiblePassword ? "text" : "password"}
-            />
-            <IconButton
-              className={classes.icon}
-              onClick={() => setVisiblePassword(!visiblePassword)}
+    <>
+      {!isRegistered ? (
+        <form action="">
+          <Grid container className={classes.grid}>
+            <Box
+              boxShadow={3}
+              width="25%"
+              borderRadius="borderRadius"
+              className={classes.box}
             >
-              {visiblePassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
-            </IconButton>
-          </div>
-          <div className={classes.buttonsDiv}>
-            <Button
-              variant="contained"
-              className={classes.button}
-              color="primary"
-              fullWidth={true}
-              onClick={registerHandler}
-              disabled={loading?.valueOf()}
-            >
-              Зарегистрироваться
-            </Button>
-          </div>
-        </Box>
-      </Grid>
-    </form>
+              <Typography
+                variant="h4"
+                component="h5"
+                className={classes.typography}
+              >
+                Регистрация нового пользователя
+              </Typography>
+              <TextField
+                label="Фамилия"
+                className={classes.textField}
+                InputProps={{
+                  classes: {
+                    input: classes.emailInput,
+                  },
+                }}
+                fullWidth={true}
+                type="text"
+              />
+              <TextField
+                label="Имя"
+                className={classes.textField}
+                InputProps={{
+                  classes: {
+                    input: classes.emailInput,
+                  },
+                }}
+                fullWidth={true}
+                type="text"
+              />
+              <TextField
+                label="Почта"
+                className={classes.textField}
+                InputProps={{
+                  classes: {
+                    input: classes.emailInput,
+                  },
+                }}
+                helperText={email ? email : ""}
+                error={!!email}
+                onChange={changeHandler}
+                name="email"
+                fullWidth={true}
+                type="email"
+              />
+              <div className={classes.passwordDiv}>
+                <TextField
+                  label="Пароль"
+                  className={classes.textField}
+                  InputProps={{
+                    classes: {
+                      input: classes.passwordInput,
+                    },
+                  }}
+                  helperText={password ? password : ""}
+                  error={!!password}
+                  onChange={changeHandler}
+                  name="password"
+                  fullWidth={true}
+                  type={visiblePassword ? "text" : "password"}
+                />
+                <IconButton
+                  className={classes.icon}
+                  onClick={() => setVisiblePassword(!visiblePassword)}
+                >
+                  {visiblePassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
+                </IconButton>
+              </div>
+              <div className={classes.buttonsDiv}>
+                <Button
+                  variant="contained"
+                  className={classes.button}
+                  color="primary"
+                  fullWidth={true}
+                  onClick={registerHandler}
+                  disabled={loading?.valueOf()}
+                >
+                  Зарегистрироваться
+                </Button>
+                <Link
+                  to="/auth"
+                  className={classNames(classes.link, {
+                    [classes.linkDisabled]: loading,
+                  })}
+                >
+                  Авторизоваться
+                </Link>
+              </div>
+            </Box>
+          </Grid>
+        </form>
+      ) : (
+        <Popup />
+      )}
+    </>
   );
 };
 
--- src/pages/Registry/theme.tsx
+++ src/pages/Registry/theme.tsx
@@ -12,6 +12,8 @@ const useRegistryStyles = makeStyles(() => ({
   },
   box: {
     padding: "50px",
+    paddingTop: "25px",
+    paddingBottom: "25px",
     width: "27%",
   },
   textField: {
@@ -29,7 +31,9 @@ const useRegistryStyles = makeStyles(() => ({
     position: "relative",
   },
   buttonsDiv: {
+    marginTop: "15px",
     display: "flex",
+    flexDirection: "column",
     justifyContent: "space-between",
     alignItems: "center",
   },
@@ -39,6 +43,32 @@ const useRegistryStyles = makeStyles(() => ({
     paddingLeft: "30px",
     textTransform: "none",
     width: "100%",
+  },
+  link: {
+    fontFamily: `"Roboto", "Helvetica", "Arial", sans-serif`,
+    marginTop: "15px",
+    paddingTop: "5px",
+    paddingBottom: "5px",
+    display: "flex",
+    width: "100%",
+    fontSize: "18px",
+    justifyContent: "space-around",
+    border: "1px solid",
+    borderColor: "#3f51b5",
+    color: "#3f51b5",
+    borderRadius: "4px",
+    textDecoration: "none",
+    "&:hover": {
+      transition: "0.4s",
+      background: "#3f51b5",
+      color: "#fff",
+    },
+  },
+  linkDisabled: {
+    pointerEvents: "none",
+    cursor: "default",
+    color: "#a6a6a6",
+    borderColor: "#e0e0e0",
   },
   icon: {
     position: "absolute",
